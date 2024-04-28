d1b8d78323f93794048dad21dda59559ddb818df
Ramdas <konala20_ug@cse.nits.ac.in>
2024-04-29 00:08:10 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about"
+    class="w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white">
+        <div class="max-w-screen-lg p-4 mx-auto flex flex-col justify-center h-full w-full">
+            <div class="pb-8">
+                <p class="text-4xl font-bold inline border-b-4 border-gray-500">
+                About</p>
+            </div>
+            <p class="text-xl mt-5">
+            I’m a motivated and passionate student with the intention of gaining admission into your renowned University.
+            To secure a creative and challenging position where I can effectively contribute my skills, and innovative ideas to
+            prove myself in the target-oriented environment. IT professional with a passion for learning and growth seeking
+            a challenging position in a forward-thinking organization to contribute expertise in technology, project
+            management, and team leadership. Committed to achieving a senior management position within the next five
+            years.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Education.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div name="education"
+    class="w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white">
+        <div class="max-w-screen-lg p-4 mx-auto flex flex-col justify-center h-full w-full">
+            <div class="pb-8">
+                <p class="text-4xl font-bold inline border-b-4 border-gray-500">
+                Education</p>
+            </div>
+            <div class="pb-8">
+              <p class="p-2"><span class="font-bold text-xl">Master science (MSITM)</span> <span className="float-right">2023 – present</span></p>
+              <hr />
+              <p class="p-2">Cumberland university</p>
+            </div>
+            <div class="pb-8">
+              <p class="p-2"> <span class="font-bold text-xl">Bachelor of Technology in Computer Science and Engineering</span> <span className="float-right">2018 – 2022</span></p>
+              <hr />
+              <p class="p-2">Vignan’s foundation for science technology and research,College of Engineering <span className="float-right">Guntur</span></p>
+              <p class="p-2">CGPA 8.45/10</p>
+            </div>
+            <div class="pb-8">
+              <p class="p-2"> <span class="font-bold text-xl">Intermediate education</span> <span className="float-right">2018</span></p>
+              <hr />
+              <p class="p-2">Sri chaitanya junior collage,College of intermediate <span className="float-right">Guntur</span></p>
+              <p class="p-2">Percentage 71.9%</p>
+            </div>
+            <div class="pb-8">
+              <p class="p-2"><span class="font-bold text-xl">Schooling</span> <span className="float-right">2016</span></p>
+              <hr />
+              <p class="p-2">Bhashyam eductional institutions,School <span className="float-right">Guntur</span></p>
+              <p class="p-2">CGPA 8.2/10</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Education
